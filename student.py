f300b7d59a5345f6a00973d464df54751f773b49
Vamsi <f20230018@hyderabad.bits-pilani.ac.in@gmail.com>
2026-05-23 12:15:18 +0530

    added new file

---
+++ student.py
@@ -0,0 +1,6 @@
+from vamsi import vamsi
+
+vamsi1 = vamsi("nani", "computer science", 3.5, False)
+
+
+print(vamsi1.name)
--- vamsi.py
+++ vamsi.py
@@ -1,3 +1,10 @@
-import app
 
-print(app.index())
+class vamsi:
+
+    def __init__(self, name,major, gpa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
+
+    

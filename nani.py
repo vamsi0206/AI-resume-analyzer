fdf313e0dfca7a8cd78e3c878a3c090c8be3b44a
Vamsi <f20230018@hyderabad.bits-pilani.ac.in@gmail.com>
2026-05-23 20:40:58 +0530

    add file

--- nani.py
+++ nani.py
@@ -1,6 +1,3 @@
-from vamsi import student
-
-student1 = student("Nani", "Computer Science", 3.5) 
-student2 = student("Nandcdci", "Science", 4.5) 
-
-print(student1.on_honor_roll())
+mytuple = ("max",28,"boston")
+print(mytuple)
+print(type(mytuple))

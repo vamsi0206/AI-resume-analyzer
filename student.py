cde094de5b2b91f57598f4bf264f70243bdeca37
Vamsi <f20230018@hyderabad.bits-pilani.ac.in@gmail.com>
2026-05-23 12:37:28 +0530

    added files

--- student.py
+++ student.py
@@ -1,6 +1,25 @@
-from vamsi import vamsi
+from Question import Question
 
-vamsi1 = vamsi("nani", "computer science", 3.5, False)
+question_prompts = [
+    "what color are apples?\n(a) red\n(b) green\n(c) yellow\n(d) all of the above\n\n",
+    "what color are bananas?\n(a) red\n(b) green\n(c) yellow\n(d) all of the above\n\n",
+    "what color are oranges?\n(a) red\n(b) green\n(c) yellow\n(d) all of the above\n\n"
+
+]
+
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "c"),
+    Question(question_prompts[2], "d"),
+]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+            print("you got " + str(score) + "/" + str(len(questions)) + " correct")
 
 
-print(vamsi1.name)
+run_test(questions)
